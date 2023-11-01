c6a112deaaab79f9f2bd8739d150b53d59c37219
Ana <algabg1@gmail.com>
2023-10-31 21:45:21 -0300

    utilizando arquivo externo

---
+++ JAVASCRIPT/js/principal.js
@@ -0,0 +1,7 @@
+//alert("olá, mundo");
+console.log("oi, mundo");
+//console.log(document.querySelector("h1")); aqui busca por uma tag
+var titulo = document.querySelector(".titulo"); //buscando por uma classe
+console.log(titulo); //imprime a variavel
+console.log(titulo.textContent); //imprime o conteudo da variavel
+titulo.textContent = "Aparecida Nutricionista"; //alterando o conteudo da variavel
